14f7b44032915f19c5342754f9b6eba3245c630e
jstrickler <jstrickler@gmail.com>
2023-04-06 10:56:46 -0400

    fixed for negative step

--- ANSWERS/fauxrange.py
+++ ANSWERS/fauxrange.py
@@ -2,22 +2,26 @@
 class fauxrange:
 
     def __init__(self, *values):
-        self._start = 0
-        self._stop = 0
-        self._step = 1
         if len(values) == 1:
-            self._stop = values[0]
+            self._start, self._stop, self._step = 0, *values, 1
         elif len(values) == 2:
-            self._start = values[0]
-            self._stop = values[1]
+            self._start, self._stop, self._step = *values, 1
         elif len(values) == 3:
-            self._start = values[0]
-            self._stop = values[1]
-            self._step = values[2]
+            self._start, self._stop, self._step = values
+        else:
+            raise TypeError("Arguments must be from 1 to 3 integers")
+
+        # make range checking function
+        end_value = self._stop
+        if self._step > 0:
+            self._out_of_range = lambda x: x >= self._stop
+        else:
+            self._out_of_range = lambda x: x <= end_value
 
     def __next__(self):
         next_value = self._start
-        if next_value > self._stop:
+        
+        if self._out_of_range(next_value):
             raise StopIteration
 
         self._start += self._step
@@ -35,3 +39,11 @@ print(list(r2))
 r3 = fauxrange(5, 101, 5)
 print(list(r3))
 
+r4 = fauxrange(10, 0, -1)
+print(list(r4))
+
+r5 = fauxrange(8, 23, 2)
+print(list(r5))
+
+r6 = fauxrange(100, 4, -5)
+print(list(r6))
